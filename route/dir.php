f079e9c6298aa8984bfc48830220482959fd2376
leaxming <leaxming@gmail.com>
2020-04-04 17:04:59 +0800

    edit template

--- route/dir.php
+++ route/dir.php
@@ -4,4 +4,7 @@ return [
     'news' => 'home/news/index?catId=8',
     'info/:id' => 'home/news/info?catId=9',
     'video/egao' => 'home/egao/index?catId=25',
+    'game$' => 'home/game/index?catId=26',
+    'game/pubg' => 'home/pubg/index?catId=27',
+    'game/wzry' => 'home/wzry/index?catId=28',
 ];
